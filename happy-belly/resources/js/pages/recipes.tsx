ec3feaec01da169e553f1da16f76445346fee66c
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 15:50:28 +0000

    added title component, added styling to page. included intro sentence.

--- happy-belly/resources/js/pages/recipes.tsx
+++ happy-belly/resources/js/pages/recipes.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import RecipeDataMap from "@/components/recipe-data-map";
+import NavBar from "@/components/navbar";
+import RecipesPageTitle from "@/components/recipes-page-title";
 
 interface Recipe {
     id: number;
@@ -33,11 +35,27 @@ export default function Recipes() {
 
     return (
         <>
-            <h1>Recipes Page</h1>
+            <div className="font-main-fredoka bg-bg-color min-h-dvh">
+                <NavBar />
+                <RecipesPageTitle />
+                <div className="flex flex-col gap-4 px-2 pb-4 text-center">
+                    <h2 className="text-3xl">Welcome to our Recipes page!</h2>
+                    <p className="text-center text-2xl">
+                        Explore our recommended recipes, and feel free to create and add your own delicious creations to your profile!
+                    </p>
+                </div>
+                {loadingRecipeData && <p>Loading Recipes...</p>}
+                {!loadingRecipeData &&
+                    <>
+                        <h3 className="text-2xl font-main-noto underline underline-offset-4 p-1 mx-2">Our Recipes</h3>
+                        <p className="italic font-main-fredoka mx-2">(Click title to see more)</p>
+                        <div className="bg-primary-color/30 mx-2 h-[550px] overflow-scroll rounded-2xl border-1 bg-[url(/images/geometric-bg-one-1000x1000.png)] bg-cover">
+                            {recipeData && <RecipeDataMap recipeData={recipeData} />}
+                        </div>
+                    </>
 
-            {loadingRecipeData && <p>Loading Recipes...</p>}
-
-            {recipeData && <RecipeDataMap recipeData={recipeData} />}
+                }
+            </div>
         </>
     );
 }
