1be0e7efb138879b6b5c806f64becdd5e518265e
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 15:49:21 +0000

    moved recipes page title and logo into its own component

---
+++ happy-belly/resources/js/components/recipes-page-title.tsx
@@ -0,0 +1,11 @@
+
+export default function RecipesPageTitle() {
+    return (
+        <>
+            <div className="grid grid-cols-[3fr_2fr] items-center py-4 px-2">
+                <h1 className="text-7xl text-primary-color rotate-x-10 -rotate-y-30 underline underline-offset-8 tracking-wide">Recipes</h1>
+                <img src="/images/logo-salad-bowl-legs-300x300.png" alt="happy salad bowl with legs" />
+            </div>
+        </>
+    )
+}
