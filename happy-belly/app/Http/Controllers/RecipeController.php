180e79217ef788355842a729e22f6efcc4206935
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 10:19:29 +0000

    created recipe controller with all function that returns JSON. Tested by creating a route

---
+++ happy-belly/app/Http/Controllers/RecipeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    public function all()
+    {
+        return Recipe::all();
+    }
+}
--- happy-belly/routes/web.php
+++ happy-belly/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,13 +8,13 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::get('recipe', [RecipeController::class, 'all']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
