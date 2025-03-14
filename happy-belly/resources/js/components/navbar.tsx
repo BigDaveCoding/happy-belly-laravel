a56f477ed7c72e92619d4bfa2319a18809928c8e
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 12:51:51 +0000

    added a little HB logo in nav bar. Linked this to homepage

--- happy-belly/resources/js/components/navbar.tsx
+++ happy-belly/resources/js/components/navbar.tsx
@@ -14,13 +14,14 @@ export default function NavBar() {
     return (
         <>
             {auth.user && (
-                <div className="flex justify-end p-2">
+                <div className="flex justify-between items-center border-b-1 border-b-primary-color p-2">
+                    <Link href={route('home')}><p className="font-main-fredoka text-2xl">HB</p></Link>
                     <i onClick={() => toggleNavBar()} className="fa-solid fa-bars w-10 cursor-pointer pl-2 text-center text-3xl"></i>
                 </div>
             )}
             {/*nav bar if user is logged in*/}
             {navBarOpen && auth.user && (
-                <div className="bg-bg-color font-main-fredoka border-primary-color absolute flex w-full flex-col justify-end border-t-1">
+                <div className="bg-bg-color font-main-fredoka  absolute flex w-full flex-col justify-end ">
                     <Link className=" border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
                         Home <i className="fa-solid fa-house w-10 pl-2 text-center text-xl"></i>
                     </Link>
@@ -41,7 +42,8 @@ export default function NavBar() {
 
             {/*nav bar if user is not logged in - just an icon that links to the login page*/}
             {!auth.user && (
-                <div className="flex justify-end p-2">
+                <div className="flex justify-between items-center p-2 border-b-1 border-b-primary-color">
+                    <Link href={route('home')}><p className="font-main-fredoka text-2xl">HB</p></Link>
                     <Link className="px-2 py-2 text-right" href={route('login')}>
                         <i className="fa-solid fa-arrow-right-to-bracket w-10 pl-2 text-center text-xl"></i>
                     </Link>
