900ce0c4c33059e9f6823cd62c8c2ce4f8e14abd
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 22:16:11 +0000

    started a component to display the features of the web app on the homepage

---
+++ happy-belly/resources/js/components/features-homepage.tsx
@@ -0,0 +1,11 @@
+
+export default function FeaturesHomepage() {
+    return (
+        <>
+            <div className="text-center relative flex flex-col overflow-hidden">
+                <p className="font-main-fredoka text-3xl bg-secondary-color/30 py-2 px-4">Features</p>
+                <img className="absolute opacity-10 w-full" src="/images/geometric-bg-one-1000x1000.png" alt="" />
+            </div>
+        </>
+    )
+}
