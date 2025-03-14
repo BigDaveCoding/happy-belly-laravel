0b34ec5edc5d985927a9377ae661bcfaa1bb51c6
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 10:58:56 +0000

    added logo image to logo component

--- happy-belly/resources/js/components/logo.tsx
+++ happy-belly/resources/js/components/logo.tsx
@@ -1,9 +1,11 @@
-
 export default function Logo() {
     return (
         <>
-            <h1 className="font-main-noto italic text-8xl text-center font-medium text-secondary-color">HAPPY</h1>
-            <h1 className="font-main-fredoka text-7xl text-center text-primary-color">BELLY</h1>
+            <div className="flex flex-col items-center justify-center p-2 tracking-widest">
+                <img src="/images/logo-avocado-500x300.png" alt="smiling avocado lying down pointing finger"/>
+                <h1 className="font-main-noto italic text-8xl text-center font-medium text-secondary-color">HAPPY</h1>
+                <h1 className="font-main-fredoka text-7xl text-center text-primary-color">BELLY</h1>
+            </div>
         </>
     )
 }
