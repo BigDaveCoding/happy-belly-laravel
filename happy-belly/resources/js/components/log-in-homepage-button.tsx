06ea246b013df8751f8ca7c52921028d092e4f9a
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 22:17:01 +0000

    unsure how to style log in button component

--- happy-belly/resources/js/components/log-in-homepage-button.tsx
+++ happy-belly/resources/js/components/log-in-homepage-button.tsx
@@ -4,8 +4,7 @@ export default function LogInHomepageButton() {
     return (
         <>
             <div className="flex flex-col justify-center items-center p-2">
-                <Link className="font-main-fredoka border-2 border-black bg-primary-color/30 text-2xl p-2 px-6 rounded-2xl
-                shadow-lg shadow-[rgba(0,0,0,0.3)_4px_4px_6px] hover:shadow-xl hover:shadow-[rgba(0,0,0,0.4)_6px_6px_8px]" href={route('login')}>
+                <Link className="font-main-fredoka border-2 border-black text-2xl p-2 px-6 rounded-2xl" href={route('login')}>
                     LOGIN / REGISTER
                 </Link>
             </div>
