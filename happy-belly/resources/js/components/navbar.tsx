2bb9a5fd5c5dd164bdd970c496a32237c307559b
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-14 11:22:56 +0000

    altered nav bar. displays a log in button if nobody logged in, displays nav bar when they are logged in

--- happy-belly/resources/js/components/navbar.tsx
+++ happy-belly/resources/js/components/navbar.tsx
@@ -11,35 +11,42 @@ export default function NavBar() {
         setNavBarOpen(!navBarOpen);
     }
 
+    // console.log(navBarOpen)
+
     return (
         <>
-            <div className="flex justify-end p-2">
-                <i onClick={() => toggleNavBar()} className="fa-solid fa-bars w-10 cursor-pointer pl-2 text-center text-3xl"></i>
-            </div>
-
-            {navBarOpen && (
-                <div id="navbar" className="absolute w-full bg-bg-color font-main-fredoka flex flex-col justify-end border-t-1 border-primary-color">
-                    <Link className="nav-link border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
+            {auth.user && (
+                <div className="flex justify-end p-2">
+                    <i onClick={() => toggleNavBar()} className="fa-solid fa-bars w-10 cursor-pointer pl-2 text-center text-3xl"></i>
+                </div>
+            )}
+            {/*nav bar if user is logged in*/}
+            {navBarOpen && auth.user && (
+                <div className="bg-bg-color font-main-fredoka border-primary-color absolute flex w-full flex-col justify-end border-t-1">
+                    <Link className=" border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
                         Home <i className="fa-solid fa-house w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="nav-link border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('recipe')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('recipe')}>
                         Recipes <i className="fa-solid fa-utensils w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="nav-link border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
                         Shopping List <i className="fa-solid fa-list-check w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="nav-link border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
                         Food Diary <i className="fa-solid fa-book w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    {auth.user ? (
-                        <Link className="nav-link border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('dashboard')}>
-                            Account <i className="fa-solid fa-circle-user w-10 pl-2 text-center text-xl"></i>
-                        </Link>
-                    ) : (
-                        <Link className="nav-link border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('login')}>
-                            Log In <i className="fa-solid fa-circle-user w-10 pl-2 text-center text-xl"></i>
-                        </Link>
-                    )}
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('dashboard')}>
+                        Account <i className="fa-solid fa-circle-user w-10 pl-2 text-center text-xl"></i>
+                    </Link>
+                </div>
+            )}
+
+            {/*nav bar if user is not logged in - just a icon that links to the login page*/}
+            {!auth.user && (
+                <div className="flex justify-end p-2">
+                    <Link className="px-2 py-2 text-right" href={route('login')}>
+                        <i className="fa-solid fa-arrow-right-to-bracket w-10 pl-2 text-center text-xl"></i>
+                    </Link>
                 </div>
             )}
         </>
