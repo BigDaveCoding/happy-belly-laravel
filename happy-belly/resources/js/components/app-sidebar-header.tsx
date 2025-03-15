ab40ec3f13fc0ef2b531a0b4820b488ae2818597
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 20:44:03 +0000

    removed sidebartrigger as I'm directing user straight to settings page, they don't need the sidebar

--- happy-belly/resources/js/components/app-sidebar-header.tsx
+++ happy-belly/resources/js/components/app-sidebar-header.tsx
@@ -1,13 +1,17 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
+import {Link} from "@inertiajs/react";
 
 export function AppSidebarHeader({ breadcrumbs = [] }: { breadcrumbs?: BreadcrumbItemType[] }) {
     return (
         <header className="border-sidebar-border/50 flex h-16 shrink-0 items-center gap-2 border-b px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
-            <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
+            <div className="flex w-full justify-between items-center gap-2">
+                {/*<SidebarTrigger className="-ml-1" />*/}
                 <Breadcrumbs breadcrumbs={breadcrumbs} />
+                <Link className="self-end" href={`/`}>
+                    <i className="fa-solid fa-house text-xl"></i>
+                </Link>
             </div>
         </header>
     );
