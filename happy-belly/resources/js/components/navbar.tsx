dbd879ec401fde508f99c91049f831afa0fc7cfc
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 13:54:35 +0000

    altered navbar component to take a property of userId. If userId is null they are not logged in etc

--- happy-belly/resources/js/components/navbar.tsx
+++ happy-belly/resources/js/components/navbar.tsx
@@ -1,10 +1,7 @@
-import { type SharedData } from '@/types';
-import { Link, usePage } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 import {useState} from "react";
 
-export default function NavBar() {
-    const { auth } = usePage<SharedData>().props
-
+export default function NavBar({userId}:{userId: number|null}) {
     const [navBarOpen, setNavBarOpen] = useState(false)
 
     function toggleNavBar() {
@@ -13,14 +10,14 @@ export default function NavBar() {
 
     return (
         <>
-            {auth.user && (
+            {userId !== null && (
                 <div className="flex justify-between items-center border-b-1 border-b-primary-color p-2">
                     <Link href={route('home')}><p className="font-main-fredoka text-2xl">HB</p></Link>
                     <i onClick={() => toggleNavBar()} className="fa-solid fa-bars w-10 cursor-pointer pl-2 text-center text-3xl"></i>
                 </div>
             )}
             {/*nav bar if user is logged in*/}
-            {navBarOpen && auth.user && (
+            {navBarOpen && userId !== null && (
                 <div className="bg-bg-color font-main-fredoka  absolute flex w-full flex-col justify-end z-40">
                     <Link className=" border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
                         Home <i className="fa-solid fa-house w-10 pl-2 text-center text-xl"></i>
@@ -41,7 +38,7 @@ export default function NavBar() {
             )}
 
             {/*nav bar if user is not logged in - just an icon that links to the login page*/}
-            {!auth.user && (
+            {userId === null && (
                 <div className="flex justify-between items-center p-2 border-b-1 border-b-primary-color">
                     <Link href={route('home')}><p className="font-main-fredoka text-2xl">HB</p></Link>
                     <Link className="px-2 py-2 text-right" href={route('login')}>
