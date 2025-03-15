9752ef00d091d7109a20db893247f8bbce7d2ba5
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 10:45:24 +0000

    function to return cooking instructions using recipe_id foreign key

--- happy-belly/app/Models/CookingInstruction.php
+++ happy-belly/app/Models/CookingInstruction.php
@@ -9,4 +9,12 @@ class CookingInstruction extends Model
 {
     /** @use HasFactory<\Database\Factories\CookingInstructionFactory> */
     use HasFactory;
+
+    protected $fillable = ['recipe_id', 'step', 'instruction'];
+
+    // Define a One-to-Many relationship (Each step belongs to one recipe)
+    public function recipe()
+    {
+        return $this->belongsTo(Recipe::class);
+    }
 }
