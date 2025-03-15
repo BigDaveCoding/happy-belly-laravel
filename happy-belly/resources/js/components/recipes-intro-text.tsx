2c7c1b2bccbf64094c7cbd25f189fdf1684e5181
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 20:48:13 +0000

    moved intro text into its own component

---
+++ happy-belly/resources/js/components/recipes-intro-text.tsx
@@ -0,0 +1,13 @@
+
+export default function RecipesIntroText() {
+    return (
+        <>
+            <div className="flex flex-col gap-4 px-2 pb-4 text-center">
+                <h2 className="text-3xl">Welcome to our Recipes page!</h2>
+                <p className="text-center text-2xl">
+                    Explore our recommended recipes, and feel free to create and add your own delicious creations to your profile!
+                </p>
+            </div>
+        </>
+    )
+}
--- happy-belly/resources/js/pages/recipes.tsx
+++ happy-belly/resources/js/pages/recipes.tsx
@@ -2,6 +2,7 @@ import RecipeDataMap from "@/components/recipe-data-map";
 import NavBar from "@/components/navbar";
 import RecipesPageTitle from "@/components/recipes-page-title";
 import { Recipe } from "@/types"
+import RecipesIntroText from "@/components/recipes-intro-text";
 
 export default function Recipes({userId, adminRecipes, userRecipes}:{userId: number|null, adminRecipes: Recipe[], userRecipes: Recipe[]}) {
 
@@ -13,13 +14,7 @@ export default function Recipes({userId, adminRecipes, userRecipes}:{userId: num
             <div className="font-main-fredoka bg-bg-color min-h-dvh">
                 <NavBar userId={userId}/>
                 <RecipesPageTitle />
-
-                <div className="flex flex-col gap-4 px-2 pb-4 text-center">
-                    <h2 className="text-3xl">Welcome to our Recipes page!</h2>
-                    <p className="text-center text-2xl">
-                        Explore our recommended recipes, and feel free to create and add your own delicious creations to your profile!
-                    </p>
-                </div>
+                <RecipesIntroText />
 
                 <h3 className="text-2xl font-main-noto underline underline-offset-4 p-2 mx-2">Our Recipes</h3>
                 <div className="bg-primary-color/30 mx-2 h-[550px] overflow-scroll rounded-2xl border-1 bg-[url(/images/geometric-bg-one-1000x1000.png)] bg-cover">
