779beac205b659766810048352a4c03b886266fb
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 10:47:33 +0000

    many to many relationship defined in function

--- happy-belly/app/Models/Recipe.php
+++ happy-belly/app/Models/Recipe.php
@@ -9,4 +9,14 @@ class Recipe extends Model
 {
     /** @use HasFactory<\Database\Factories\RecipeFactory> */
     use HasFactory;
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class, 'ingredient_recipe', 'recipe_id', 'ingredient_id');
+    }
+
+    public function cookingInstructions()
+    {
+        return $this->hasMany(CookingInstruction::class);
+    }
 }
