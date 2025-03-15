c856e9a49c4cc5f90907371c2772af9c1c0b8068
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 15:32:35 +0000

    removed unused code

--- happy-belly/resources/js/pages/homepage.tsx
+++ happy-belly/resources/js/pages/homepage.tsx
@@ -1,12 +1,9 @@
 import Logo from "@/components/logo";
 import NavBar from "@/components/navbar";
 import LogInHomepageButton from "@/components/log-in-homepage-button";
-import { type SharedData } from '@/types';
-import { usePage } from '@inertiajs/react';
 import IntroHomepage from "@/components/intro-homepage";
 import FeaturesHomepage from "@/components/features-homepage";
 export default function homepage({userId}:{userId: number|null}) {
-    const { auth } = usePage<SharedData>().props
     console.log(userId)
 
     return (
