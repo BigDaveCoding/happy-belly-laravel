13560440c3dbb918ec0c0e797d1d5ebda9d5bb74
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-15 16:15:50 +0000

    changed href values of links

--- happy-belly/resources/js/components/navbar.tsx
+++ happy-belly/resources/js/components/navbar.tsx
@@ -12,26 +12,26 @@ export default function NavBar({userId}:{userId: number|null}) {
         <>
             {userId !== null && (
                 <div className="flex justify-between items-center border-b-1 border-b-primary-color p-2">
-                    <Link href={route('home')}><p className="font-main-fredoka text-2xl">HB</p></Link>
+                    <Link href={`/`}><p className="font-main-fredoka text-2xl">HB</p></Link>
                     <i onClick={() => toggleNavBar()} className="fa-solid fa-bars w-10 cursor-pointer pl-2 text-center text-3xl"></i>
                 </div>
             )}
             {/*nav bar if user is logged in*/}
             {navBarOpen && userId !== null && (
                 <div className="bg-bg-color font-main-fredoka  absolute flex w-full flex-col justify-end z-40">
-                    <Link className=" border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
+                    <Link className=" border-b-primary-color border-b-1 px-2 py-2 text-right" href={`/`}>
                         Home <i className="fa-solid fa-house w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('recipes')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={`/recipes`}>
                         Recipes <i className="fa-solid fa-utensils w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={`/`}>
                         Shopping List <i className="fa-solid fa-list-check w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('home')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={`/`}>
                         Food Diary <i className="fa-solid fa-book w-10 pl-2 text-center text-xl"></i>
                     </Link>
-                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={route('dashboard')}>
+                    <Link className="border-b-primary-color border-b-1 px-2 py-2 text-right" href={`dashboard`}>
                         Account <i className="fa-solid fa-circle-user w-10 pl-2 text-center text-xl"></i>
                     </Link>
                 </div>
@@ -40,8 +40,8 @@ export default function NavBar({userId}:{userId: number|null}) {
             {/*nav bar if user is not logged in - just an icon that links to the login page*/}
             {userId === null && (
                 <div className="flex justify-between items-center p-2 border-b-1 border-b-primary-color">
-                    <Link href={route('home')}><p className="font-main-fredoka text-2xl">HB</p></Link>
-                    <Link className="px-2 py-2 text-right" href={route('login')}>
+                    <Link href={`/`}><p className="font-main-fredoka text-2xl">HB</p></Link>
+                    <Link className="px-2 py-2 text-right" href={`/login`}>
                         <i className="fa-solid fa-arrow-right-to-bracket w-10 pl-2 text-center text-xl"></i>
                     </Link>
                 </div>
