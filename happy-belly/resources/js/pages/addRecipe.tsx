d2a88eb6271e69748bbd6b81d4e53db6029a3aa6
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-17 12:29:57 +0000

    working on error handling in form.

--- happy-belly/resources/js/pages/addRecipe.tsx
+++ happy-belly/resources/js/pages/addRecipe.tsx
@@ -1,16 +1,34 @@
 import NavBar from "@/components/navbar";
 import {FormEvent, useEffect, useState} from "react";
 
+interface RecipeFormData {
+    recipe_name: string;
+    recipe_description: string;
+    recipe_image: string;
+    recipe_cooking_time: string;
+    recipe_serves: string;
+}
+
 export default function addRecipe() {
 
     const [csrfToken, setCsrfToken] = useState<string | null>(null);
 
-    const [recipeData, setRecipeData] = useState<object>({
+    const [formErrors, setFormErrors] = useState<boolean>(false);
+
+    const [recipeData, setRecipeData] = useState<RecipeFormData>({
         'recipe_name' : '',
         'recipe_description' : '',
         'recipe_image' : '',
         'recipe_cooking_time' : '',
-        'recipe_serves' : 0
+        'recipe_serves' : '0'
+    })
+
+    const [errors, setRecipeErrors] = useState<RecipeFormData>({
+        'recipe_name' : 'Recipe Name must be longer than 4 characters',
+        'recipe_description' : 'Must be between 50 and 5000 characters',
+        'recipe_image' : '',
+        'recipe_cooking_time' : 'Must be a number & above 0',
+        'recipe_serves' : 'must be a number & above 0'
     })
 
     async function getToken() {
@@ -30,50 +48,93 @@ export default function addRecipe() {
         }));
     }
 
-    async function handleFormSubmit(e: FormEvent) {
-        e.preventDefault()
-        inputRecipeData(e)
-        const token = await getToken()
-        setCsrfToken(token)
-
-        console.log("token in handle submit form",token)
-        console.log("recipe data:",recipeData)
-
-
+    function formErrorsExist(e: FormEvent) {
+        if (recipeData.recipe_name.length < 4 ||
+            (recipeData.recipe_description.length < 50 || recipeData.recipe_description.length > 500) ||
+            (isNaN(parseInt(recipeData.recipe_cooking_time)) || parseInt(recipeData.recipe_cooking_time) <= 0) ||
+            (isNaN(parseInt(recipeData.recipe_serves)) || parseInt(recipeData.recipe_serves) <= 0)){
+            e.preventDefault()
+            setFormErrors(true)
+        }
     }
 
     useEffect(() => {
         getToken()
-    }, [])
+        console.log("form updating, getting token", csrfToken)
+    }, [formErrorsExist])
+
+    // console.log(recipeData)
 
     return (
         <>
             <NavBar userId={1} />
             <h1>Add Recipe</h1>
             <form
-                onChange={(e) => inputRecipeData(e)}
-                onSubmit={(e) => handleFormSubmit(e)}
                 className="grid grid-cols-[1fr_2fr] gap-2 items-center mx-4"
                 method="post"
                 action="/recipe/add"
+                onChange={(e) => inputRecipeData(e)}
+                onSubmit={(e) => formErrorsExist(e)}
             >
                 {csrfToken && <input type="hidden" name="_token" value={csrfToken} />}
                 <label htmlFor="recipe_name">Recipe Name :</label>
-                <input className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded" type="text" name="recipe_name" placeholder="Enchiladas" />
+                <input
+                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    type="text" name="recipe_name"
+                    placeholder="Enchiladas"
+                />
+                {formErrors && recipeData.recipe_name.length < 4 &&
+                    <p className="col-span-2 text-md text-red-500">{errors.recipe_name}</p>
+                }
 
                 <label htmlFor="recipe_description">Description :</label>
-                <textarea className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded" name="recipe_description" placeholder="How would you describe your recipe?" ></textarea>
+                <textarea
+                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    name="recipe_description"
+                    placeholder="How would you describe your recipe?"
+                >
+                </textarea>
+                {formErrors &&
+                    (recipeData.recipe_description.length < 50 || recipeData.recipe_description.length > 5000) &&
+                    <p className="col-span-2 text-md text-red-500">{errors.recipe_description}</p>
+                }
+
 
                 <label htmlFor="recipe_image">Image URL :</label>
-                <input className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded" type="text" name="recipe_image" placeholder="www.image.com" />
+                <input
+                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    type="text"
+                    name="recipe_image"
+                    placeholder="www.image.com"
+                />
 
                 <label htmlFor="recipe_cooking_time">Cooking Time :<br/><i>(Minutes)</i></label>
-                <input className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded" type="text" name="recipe_cooking_time" placeholder="30" />
+                <input
+                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    type="text" name="recipe_cooking_time"
+                    placeholder="30"
+                />
+                {formErrors &&
+                    (isNaN(parseInt(recipeData.recipe_cooking_time)) || parseInt(recipeData.recipe_cooking_time) <= 0) &&
+                    <p className="col-span-2 text-md text-red-500">{errors.recipe_cooking_time}</p>
+                }
 
                 <label htmlFor="recipe_serves">Serves :</label>
-                <input className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded" type="number" name="recipe_serves" placeholder="4" />
+                <input
+                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    type="number"
+                    name="recipe_serves" placeholder="4"
+                />
+                {formErrors &&
+                    (isNaN(parseInt(recipeData.recipe_serves)) || parseInt(recipeData.recipe_serves) <= 0) &&
+                    <p className="col-span-2 text-md text-red-500">{errors.recipe_serves}</p>
+                }
 
-                <input className="col-span-2 border-1 p-2 w-8/12 bg-primary-color justify-self-center rounded inset-shadow-sm inset-shadow-black/30" type="submit" value="Add recipe" />
+                <input
+                    className="col-span-2 border-1 p-2 w-8/12 bg-primary-color justify-self-center rounded inset-shadow-sm inset-shadow-black/30"
+                    type="submit"
+                    value="Add recipe"
+                />
 
             </form>
         </>
