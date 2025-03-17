52445823a16fcbb4f06a7981b66565f3b544aea1
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-17 13:55:26 +0000

    added add and remove ingredient functionality

--- happy-belly/resources/js/pages/addRecipe.tsx
+++ happy-belly/resources/js/pages/addRecipe.tsx
@@ -100,7 +100,7 @@ export default function addRecipe() {
             <NavBar userId={1} />
             <h1>Add Recipe</h1>
             <form
-                className="grid grid-cols-[1fr_2fr] gap-2 items-center mx-4"
+                className="mx-4 grid grid-cols-[1fr_2fr] items-center gap-2"
                 method="post"
                 action="/recipe/add"
                 onChange={(e) => inputRecipeData(e)}
@@ -109,109 +109,117 @@ export default function addRecipe() {
                 {csrfToken && <input type="hidden" name="_token" value={csrfToken} />}
                 <label htmlFor="recipe_name">Recipe Name :</label>
                 <input
-                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
-                    type="text" name="recipe_name"
+                    className="rounded border-1 border-black p-2 inset-shadow-sm inset-shadow-slate-300"
+                    type="text"
+                    name="recipe_name"
                     placeholder="Enchiladas"
                 />
-                {formErrors && recipeData.recipe_name.length < 4 &&
-                    <p className="col-span-2 text-md text-red-500">{errors.recipe_name}</p>
-                }
+                {formErrors && recipeData.recipe_name.length < 4 && <p className="text-md col-span-2 text-red-500">{errors.recipe_name}</p>}
 
                 <label htmlFor="recipe_description">Description :</label>
                 <textarea
-                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    className="rounded border-1 border-black p-2 inset-shadow-sm inset-shadow-slate-300"
                     name="recipe_description"
                     placeholder="How would you describe your recipe?"
-                >
-                </textarea>
-                {formErrors &&
-                    (recipeData.recipe_description.length < 50 || recipeData.recipe_description.length > 5000) &&
-                    <p className="col-span-2 text-md text-red-500">{errors.recipe_description}</p>
-                }
+                ></textarea>
+                {formErrors && (recipeData.recipe_description.length < 50 || recipeData.recipe_description.length > 5000) && (
+                    <p className="text-md col-span-2 text-red-500">{errors.recipe_description}</p>
+                )}
 
-                <label className={`col-span-2`} htmlFor="recipe_image">Image:</label>
+                <label className={`col-span-2`} htmlFor="recipe_image">
+                    Image:
+                </label>
                 <input
-                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded col-span-2"
+                    className="col-span-2 rounded border-1 border-black p-2 inset-shadow-sm inset-shadow-slate-300"
                     type="file"
                     name="recipe_image"
                 />
-                {recipeData.recipe_image.length > 0 && <img className="col-span-2" src={recipeData.recipe_image} alt='' />}
+                {recipeData.recipe_image.length > 0 && <img className="col-span-2" src={recipeData.recipe_image} alt="" />}
 
-                <label htmlFor="recipe_cooking_time">Cooking Time :<br/><i>(Minutes)</i></label>
+                <label htmlFor="recipe_cooking_time">
+                    Cooking Time :<br />
+                    <i>(Minutes)</i>
+                </label>
                 <input
-                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
-                    type="text" name="recipe_cooking_time"
+                    className="rounded border-1 border-black p-2 inset-shadow-sm inset-shadow-slate-300"
+                    type="text"
+                    name="recipe_cooking_time"
                     placeholder="30"
                 />
-                {formErrors &&
-                    (isNaN(parseInt(recipeData.recipe_cooking_time)) || parseInt(recipeData.recipe_cooking_time) <= 0) &&
-                    <p className="col-span-2 text-md text-red-500">{errors.recipe_cooking_time}</p>
-                }
+                {formErrors && (isNaN(parseInt(recipeData.recipe_cooking_time)) || parseInt(recipeData.recipe_cooking_time) <= 0) && (
+                    <p className="text-md col-span-2 text-red-500">{errors.recipe_cooking_time}</p>
+                )}
 
                 <label htmlFor="recipe_serves">Serves :</label>
                 <input
-                    className="border-1 border-black inset-shadow-sm inset-shadow-slate-300 p-2 rounded"
+                    className="rounded border-1 border-black p-2 inset-shadow-sm inset-shadow-slate-300"
                     type="number"
-                    name="recipe_serves" placeholder="4"
+                    name="recipe_serves"
+                    placeholder="4"
                 />
-                {formErrors &&
-                    (isNaN(parseInt(recipeData.recipe_serves)) || parseInt(recipeData.recipe_serves) <= 0) &&
-                    <p className="col-span-2 text-md text-red-500">{errors.recipe_serves}</p>
-                }
+                {formErrors && (isNaN(parseInt(recipeData.recipe_serves)) || parseInt(recipeData.recipe_serves) <= 0) && (
+                    <p className="text-md col-span-2 text-red-500">{errors.recipe_serves}</p>
+                )}
+
+                <hr className="col-span-2 border-black my-2" />
 
                 <h2 className="col-span-2">Ingredients</h2>
 
-                {ingredientData.map((ingredient, index:number) => {
-                    console.log(index)
-                    console.log(ingredient)
+                {ingredientData.map((ingredient, index: number) => {
+                    console.log(index);
+                    console.log(ingredient);
                     return (
                         <>
                             <div className="col-span-2" key={index + 1}>
-                                <div className="grid grid-cols-2 gap-2 border-1 border-black/50 rounded p-2">
+                                <div className="grid grid-cols-2 gap-2 rounded border-1 border-black/50 p-2">
                                     <input
-                                        className="col-span-2 border border-black p-2 rounded inset-shadow-sm inset-shadow-slate-300"
+                                        className="col-span-2 rounded border border-black p-2 inset-shadow-sm inset-shadow-slate-300"
                                         type="text"
                                         name="ingredient_name"
                                         value={ingredient.ingredient_name}
                                         placeholder="ingredient"
-                                        onChange={(e) => inputIngredientData(index ,e)}
+                                        onChange={(e) => inputIngredientData(index, e)}
                                     />
                                     <input
-                                        className="border border-black p-2 rounded inset-shadow-sm inset-shadow-slate-300"
+                                        className="rounded border border-black p-2 inset-shadow-sm inset-shadow-slate-300"
                                         type="number"
                                         name="ingredient_quantity"
                                         value={ingredient.ingredient_quantity}
                                         placeholder="quantity"
-                                        onChange={(e) => inputIngredientData(index ,e)}
+                                        onChange={(e) => inputIngredientData(index, e)}
                                     />
                                     <input
-                                        className="border border-black p-2 rounded inset-shadow-sm inset-shadow-slate-300"
+                                        className="rounded border border-black p-2 inset-shadow-sm inset-shadow-slate-300"
                                         type="text"
                                         name="ingredient_unit"
                                         value={ingredient.ingredient_unit}
                                         placeholder="unit"
-                                        onChange={(e) => inputIngredientData(index ,e)}
+                                        onChange={(e) => inputIngredientData(index, e)}
                                     />
                                 </div>
                             </div>
                         </>
-                    )
+                    );
                 })}
 
-                <button type="button" onClick={() => addIngredient()}>
-                    Add Ingredient
-                </button>
-                <button type="button" onClick={() => removeIngredient()}>
-                    Remove Ingredient
+                {/*add ingredient button*/}
+                <button className="" type="button" onClick={() => addIngredient()}>
+                    <i className="fa-solid fa-plus"></i>
                 </button>
 
+                {/*remove ingredient button*/}
+                { ingredientData.length > 1 &&
+                    <button className="" type="button" onClick={() => removeIngredient()}>
+                        <i className="fa-solid fa-minus"></i>
+                    </button>
+                }
+
                 <input
-                    className="col-span-2 border-1 p-2 w-8/12 bg-primary-color justify-self-center rounded inset-shadow-sm inset-shadow-black/30"
+                    className="bg-primary-color col-span-2 w-8/12 justify-self-center rounded border-1 p-2 inset-shadow-sm inset-shadow-black/30"
                     type="submit"
                     value="Add recipe"
                 />
-
             </form>
         </>
-    )
+    );
 }
