ee346cf9fad5a703aaed97e9ab6e9eeb60b32d85
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-18 15:35:18 +0000

    added function to update the ingredientData state, I will use this state to handle errors

--- happy-belly/resources/js/hooks/use-ingredient-form-data.tsx
+++ happy-belly/resources/js/hooks/use-ingredient-form-data.tsx
@@ -1,15 +1,28 @@
 import {IngredientFormData} from "@/types";
-import {useState} from "react";
+import {FormEvent, useState} from "react";
 
 export function useIngredientFormData() {
     const [ingredientData, setIngredientData] = useState<IngredientFormData[]>([
-        {ingredient_name: '', ingredient_quantity: '', ingredient_unit: ''}
+        {ingredient_name: '', ingredient_quantity: '', ingredient_unit: '', ingredient_allergen : 0}
     ])
+
+    function updateIngredientData(e: FormEvent, index: number) {
+        const {name, value} = e.target as HTMLInputElement
+        setIngredientData((prevData : IngredientFormData[]) : IngredientFormData[] => {
+            const newData : IngredientFormData[] = [...prevData]
+            // Because laravel needs [] in name attribute,
+            // I'm slicing name to remove '[]'
+            // Goes from 'ingredient_name[]' to 'ingredient_name'
+            // Matching my ingredientData
+            newData[index] = {...newData[index], [name.slice(0, -2)] : value}
+            return newData
+        })
+    }
 
     function addIngredient(): void {
         setIngredientData([
             ...ingredientData,
-            {ingredient_name: '', ingredient_quantity: '', ingredient_unit: ''}
+            {ingredient_name: '', ingredient_quantity: '', ingredient_unit: '', ingredient_allergen : 0}
         ])
     }
 
@@ -18,5 +31,5 @@ export function useIngredientFormData() {
         setIngredientData(removedIngredientArray)
     }
 
-    return {ingredientData, addIngredient, removeIngredient}
+    return {ingredientData, addIngredient, removeIngredient, updateIngredientData}
 }
